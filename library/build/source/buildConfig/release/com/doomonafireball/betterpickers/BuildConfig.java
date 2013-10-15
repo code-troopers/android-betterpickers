f897a4c5f921b4dc01eda7ba503761f2417933eb
Derek Brameyer <derek.brameyer@gmail.com>
2013-10-15 14:19:44 -0400

    Gradle build information in progress.

---
+++ library/build/source/buildConfig/release/com/doomonafireball/betterpickers/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.doomonafireball.betterpickers;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = false;
+
+}
