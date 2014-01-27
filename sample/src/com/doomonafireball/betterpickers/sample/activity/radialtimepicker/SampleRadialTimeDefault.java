8b7a6a5d3aa28bcebab4c742c6df7caa65fe4eb5
arichiardi <a.richiardi.work@gmail.com>
2014-01-26 20:32:53 -0800

    Fixed NullPointerException in RadialTimePickerDialog fragment when embedded (Issue #57)

--- sample/src/com/doomonafireball/betterpickers/sample/activity/radialtimepicker/SampleRadialTimeDefault.java
+++ sample/src/com/doomonafireball/betterpickers/sample/activity/radialtimepicker/SampleRadialTimeDefault.java
@@ -1,18 +1,19 @@
 package com.doomonafireball.betterpickers.sample.activity.radialtimepicker;
 
+import org.joda.time.DateTime;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
 import com.doomonafireball.betterpickers.radialtimepicker.RadialPickerLayout;
 import com.doomonafireball.betterpickers.radialtimepicker.RadialTimePickerDialog;
 import com.doomonafireball.betterpickers.sample.R;
 import com.doomonafireball.betterpickers.sample.activity.BaseSampleActivity;
-
-import org.joda.time.DateTime;
-
-import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
-import android.text.format.DateFormat;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 /**
  * User: derek Date: 3/17/13 Time: 3:59 PM
@@ -22,26 +23,42 @@ public class SampleRadialTimeDefault extends BaseSampleActivity
 
     private TextView text;
     private Button button;
-
+    private boolean mHasDialogFrame;
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.text_and_button);
+        
+        if (savedInstanceState == null) {
+            mHasDialogFrame = (ViewGroup) findViewById(R.id.frame) == null ? false : true;
+        }
 
         text = (TextView) findViewById(R.id.text);
         button = (Button) findViewById(R.id.button);
 
-        text.setText("--");
+        if (mHasDialogFrame) text.setText("|");
+        else text.setText("--");
+        
         button.setText("Set Time");
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FragmentManager fm = getSupportFragmentManager();
                 DateTime now = DateTime.now();
+                
                 RadialTimePickerDialog timePickerDialog = RadialTimePickerDialog
                         .newInstance(SampleRadialTimeDefault.this, now.getHourOfDay(), now.getMinuteOfHour(),
                                 DateFormat.is24HourFormat(SampleRadialTimeDefault.this));
-                timePickerDialog.show(fm, "fragment_time_picker_name");
+                
+                if (mHasDialogFrame) {
+                    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                    
+                    ft.add(R.id.frame, timePickerDialog, "fragment_time_picker_name")
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                        .commit();
+                } else {
+                    timePickerDialog.show(getSupportFragmentManager(), "fragment_time_picker_name");
+                }
             }
         });
     }
